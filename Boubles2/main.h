49fa8c03999e19034d808c207a58f52b57327047
gunnlaugur <gju@kth.se>
2015-04-17 10:08:11 +0200

    Projekt1

---
+++ Boubles2/main.h
@@ -0,0 +1,14 @@
+#ifdef _WIN32
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_net.h>
+#include <SDL_image.h>
+
+#endif // _WIN32
+
+#ifdef __APPLE__
+
+#include <SDL2/SDL.h>
+#include <SDL2_net/SDL_net.h>
+
+#endif // __APPLE__
