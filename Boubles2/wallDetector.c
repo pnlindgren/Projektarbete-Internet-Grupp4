9c830adad33612a3f25cc7b367d47e42d6ab6b19
gunnlaugur <gju@kth.se>
2015-04-26 17:02:01 +0200

    Update 26

--- Boubles2/wallDetector.c
+++ Boubles2/wallDetector.c
@@ -7,5 +7,28 @@
 // Baserad på lazyfoo.net/SDL_tutorials/lesson17/
 bool wallDetection()
 {
-    return SDL_HasIntersection(&character_rect, &ghost_rect);
+    bool collision = false;
+
+    SDL_Rect left_wall, right_wall;
+
+    left_wall.x = 16;
+    left_wall.y = 0;
+    left_wall.w = 1;
+    left_wall.h = 480;
+
+    left_wall.x = 624;
+    left_wall.y = 0;
+    left_wall.w = 1;
+    left_wall.h = 480;
+
+    if(SDL_HasIntersection(&character_rect, &ghost_rect))
+    {
+        collision = true;
+    }
+    else if(SDL_HasIntersection(&left_wall, &ghost_rect))
+    {
+        collision = true;
+    }
+
+    return collision;
 }
