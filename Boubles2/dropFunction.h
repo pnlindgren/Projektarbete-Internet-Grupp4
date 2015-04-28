b375799307f6639bf682fe5cdc2e080d95046431
carlitos84 <cgaldo@kth.se>
2015-04-28 15:26:25 +0200

    dropFunction

---
+++ Boubles2/dropFunction.h
@@ -0,0 +1,12 @@
+#ifndef DROPFUNCTION
+#define DROPFUNCTION
+
+#define RIGHT 1
+#define LEFT 0
+
+void dropFunction(void *pointer);
+
+extern SDL_Rect character_rect;
+extern SDL_Rect ghost_rect;
+
+#endif // DROPFUNCTION
