3f96b03c08995a357b56ca787fb89e49ae90b7cd
selmachowdhury <selmachowdhury@live.se>
2015-04-29 10:11:10 +0200

    server

--- Servern/server_main.c
+++ Servern/server_main.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
+#include "server_main.h"
+
+void main(){
+
+    SDLNet_ResolveHost(*IPaddress, NULL, 4523);
+
+    socket = SDLNet_TCP_Open(*IPaddress);
+
+    while(1)
+    {
+        if(Klientsockets[1] = SDLNet_TCP_Accept(socket))
+            break;
+        else
+            SDL_Delay(200);
+
+    }
+    return 0;
+
+
+}
--- Servern/server_main.h
+++ Servern/server_main.h
@@ -1,1 +1,34 @@
 
+#ifdef _WIN32
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_net.h>
+#include <SDL2/SDL_image.h>
+
+#endif // _WIN32
+
+#ifdef __APPLE__
+
+#include <SDL2/SDL.h>
+#include <SDL2_net/SDL_net.h>
+#include <SDL2_image/SDL_image.h>
+
+#endif // __APPLE__
+
+#ifndef CONSTANTS
+#define CONSTANTS
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+enum KeyPressSurfaces
+{
+    KEY_PRESS_SURFACE_DEFAULT,
+    KEY_PRESS_SURFACE_UP,
+    KEY_PRESS_SURFACE_DOWN,
+    KEY_PRESS_SURFACE_LEFT,
+    KEY_PRESS_SURFACE_RIGHT,
+    KEY_PRESS_SURFACE_TOTAL
+};
+
+#endif
