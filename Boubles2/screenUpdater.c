bc25c909bc60f31d3ce080c9cb42bcd5ea4582ae
selmachowdhury <selmachowdhury@live.se>
2015-05-05 10:49:06 +0200

    screenUpdaterSelma

--- Boubles2/screenUpdater.c
+++ Boubles2/screenUpdater.c
@@ -19,16 +19,28 @@ int screenUpdateFunction(void * pointer)
         {
             quit = true;
         }
+         if(bubble_view==true){
+
+            SDL_RenderClear(gRenderer);
+            SDL_RenderCopyEx(gRenderer, mBackground, &gSpriteClips[6],&background_rect , 0, NULL, SDL_FLIP_NONE);
+            SDL_RenderCopyEx(gRenderer, mBlueCrocodile, &gSpriteClips[frame],&character_rect , 0, NULL, flip);
+            SDL_RenderCopyEx(gRenderer, mGhost, &gSpriteClips[8],&ghost_rect , 0, NULL, SDL_FLIP_NONE);
+            SDL_RenderCopyEx(gRenderer, mBubble, &gSpriteClips[9],&bubble_rect , 0, NULL, SDL_FLIP_NONE);
+            SDL_RenderPresent(gRenderer);
+        }
     }
 
     SDL_RenderCopyEx(gRenderer, mBackground, &gSpriteClips[6],&background_rect , 0, NULL, SDL_FLIP_NONE);
     SDL_RenderCopyEx(gRenderer, mGhost, &gSpriteClips[8],&ghost_rect , 0, NULL, SDL_FLIP_NONE);
     SDL_RenderPresent(gRenderer);
 
-    SDL_Delay(1000);
+    SDL_Delay(100);
 
     SDL_RenderCopyEx(gRenderer, mBackground, &gSpriteClips[6],&background_rect , 0, NULL, SDL_FLIP_NONE);
     SDL_RenderCopyEx(gRenderer, mBlueCrocodile, &gSpriteClips[frame],&character_rect , 0, NULL, flip);
     SDL_RenderCopyEx(gRenderer, mGhost, &gSpriteClips[8],&ghost_rect , 0, NULL, SDL_FLIP_NONE);
     SDL_RenderPresent(gRenderer);
-}
+
+
+    }
+
