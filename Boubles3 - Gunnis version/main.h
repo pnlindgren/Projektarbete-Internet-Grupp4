4c0587ea764a71ffcb9559f75f923a5ec7560942
gunnlaugur <gju@kth.se>
2015-05-25 12:37:24 +0200

    Update 68 - main.h

--- Boubles3 - Gunnis version/main.h
+++ Boubles3 - Gunnis version/main.h
@@ -3,6 +3,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_net.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
 
 #endif // _WIN32
 
@@ -11,6 +12,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2_net/SDL_net.h>
 #include <SDL2_image/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
 
 #endif // __APPLE__
 
